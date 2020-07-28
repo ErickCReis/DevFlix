df2864be5d4aca05cbbd7a9fc9477585bbdb69fe
ErickCReis <erickcorreareis@gmail.com>
2020-07-28 10:58:00 -0300

    Aula 02

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import dadosIniciais from './data/dados_iniciais.json'
-import Menu from './components/Menu'
-import BannerMain from './components/BannerMain'
-import Carousel from './components/Carousel'
-import Footer from './components/Footer'
-
+import dadosIniciais from '../../data/dados_iniciais.json'
+import PageDefault from '../../components/PageDefault';
+import BannerMain from '../../components/BannerMain'
+import Carousel from '../../components/Carousel'
 
 function App() {
   return (
-    <div>
-      <Menu />
-      
-      <BannerMain 
+    <>
+      <PageDefault>
+
+        <BannerMain 
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription="Teste"
@@ -19,14 +17,12 @@ function App() {
 
       {dadosIniciais.categorias.map((categoria) => {
         return <Carousel 
-          ignoreFirstVideo
           category={categoria}
         />
       })}
 
-      <Footer />
-
-    </div>
+      </PageDefault>
+    </>
   );
 }
 
