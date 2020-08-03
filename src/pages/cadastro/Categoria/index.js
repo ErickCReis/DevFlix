9d9dc65c70b3673d5f930e66d085fd6287a6b69c
ErickCReis <erickcorreareis@gmail.com>
2020-08-03 01:24:32 -0300

    Aula 04 - Ajax, validação e o back-end

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,89 +1,102 @@
-import React, { useState } from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
-function CadastroCategoria(){
-    const valoresIniciais = {
-      nome: '',
-      descricao: '',
-      cor: '#000'
-    }
+function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '#000',
+  };
 
   const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
 
   function saveValue(chave, valor) {
-    //chaves: nome, descricao, cor
+    // chaves: nome, descricao, cor
     setValues({
       ...values,
       [chave]: valor,
-    })
+    });
   }
 
-  function handleChanges (data) {
+  function handleChanges(data) {
     saveValue(data.target.getAttribute('name'), data.target.value);
   }
 
-  
+  useEffect(() => {
+    console.log('Teste Use Effect');
+    const URL = 'http://localhost:8080/categorias/';
+
+    fetch(URL)
+      .then(async (resposta) => {
+        const data = await resposta.json();
+        console.log(data);
+        setCategorias([
+          ...data,
+        ]);
+      });
+  }, []);
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {values.nome} </h1>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
 
       <form onSubmit={function handleSubmit(data) {
         data.preventDefault();
         setCategorias([
           ...categorias,
-          values
+          values,
         ]);
         setValues(valoresIniciais);
-      }}>
+      }}
+      >
 
-        <FormField 
+        <FormField
           label="Nome da Categoria"
           type="text"
           name="nome"
-          value={values.nome} 
+          value={values.nome}
           onChange={handleChanges}
         />
 
-
-        <FormField 
+        <FormField
           label="Descrição"
           type="textarea"
-          name="descrição"
-          value={values.descricao} 
+          name="descricao"
+          value={values.descricao}
           onChange={handleChanges}
         />
 
-
-        <FormField 
+        <FormField
           label="Cor"
           type="color"
           name="cor"
-          value={values.cor} 
+          value={values.cor}
           onChange={handleChanges}
         />
 
-        <button>
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </form>
 
       <ul>
-        {categorias.map((categoria, indice) => {
-          return (
-            <li key={`${categorias.nome}${indice}`}>{categoria.nome}</li>
-          )
-        })}
+        {categorias.map((categoria) => (
+          <li key={`${categorias.nome}`}>{categoria.nome}</li>
+        ))}
       </ul>
 
       <Link to="/">
         Home
       </Link>
     </PageDefault>
-  )
+  );
 }
 
 export default CadastroCategoria;
-  
